aa0b055c074ae36c8d723d71a7b14bda72864a1b
Ryan Sinclair <sinky1903@aol.com>
2017-01-10 15:23:18 +0000

    added multiply and length_of_name functions

--- next.rb
+++ next.rb
@@ -3,3 +3,15 @@ def next_number(number)
 end
 
 puts next_number (3)
+
+def multiply(number_1, number_2)
+  return number_1 * number_2
+end
+
+puts multiply(2, 3)
+
+def length_of_name(name1, name2)
+  return name1.length + name2.length
+end
+
+puts length_of_name("Ryan", "Sinclair")
